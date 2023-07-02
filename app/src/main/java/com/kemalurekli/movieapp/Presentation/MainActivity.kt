cbf4f52434254bfa06ad2bb0c1837566ce3824a0
kemalurekli <kemalurekli@gmail.com>
2023-07-02 15:48:54 +0300

    Retrofit Worked!

--- app/src/main/java/com/kemalurekli/movieapp/Presentation/MainActivity.kt
+++ app/src/main/java/com/kemalurekli/movieapp/Presentation/MainActivity.kt
@@ -3,44 +3,37 @@ package com.kemalurekli.movieapp.Presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kemalurekli.movieapp.Presentation.ui.theme.MovieAppTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.kemalurekli.movieapp.Presentation.MovieDetails.views.MovieDetailScreen
+import com.kemalurekli.movieapp.Presentation.Movies.views.MovieScreen
+import com.kemalurekli.movieapp.Presentation.ui.theme.MovieDatabaseComposeTheme
+import com.kemalurekli.movieapp.Util.Constants.IMDB_ID
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            MovieDatabaseComposeTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController,
+                        startDestination = Screen.MovieScreen.route
+                    ) {
+                        composable(route = Screen.MovieScreen.route) {
+                            MovieScreen(navController = navController)
+                        }
+                        composable(route = Screen.MovieDetailScreen.route+"/{${IMDB_ID}}") {
+                            MovieDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/kemalurekli/movieapp/Util/Constants.kt
+++ app/src/main/java/com/kemalurekli/movieapp/Util/Constants.kt
@@ -3,4 +3,5 @@ package com.kemalurekli.movieapp.Util
 object Constants {
     const val API_KEY = "cdf420ee"
     const val BASE_URL = "https://www.omdbapi.com/"
+    const val IMDB_ID = "imdb_id"
 }
